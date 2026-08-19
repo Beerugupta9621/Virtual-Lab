62822029f3651850cf809ddafbaa29b5849985cc
Beerugupta9621 <beeru9621449607@gmail.com>
2026-08-19 23:04:37 +0530

    feat: integrate collisions into physics engine

--- frontend/src/physics/PhysicsEngine.js
+++ frontend/src/physics/PhysicsEngine.js
@@ -1,3 +1,6 @@
+import checkCircleCollision from "./Collision";
+import resolveCollision from "./CollisionResolver";
+
 class PhysicsEngine {
     constructor(width, height) {
         this.width = width;
@@ -6,6 +9,8 @@ class PhysicsEngine {
         this.gravity = 9.8;
 
         this.objects = [];
+
+        this.collisionCount = 0;
     }
 
     addObject(object) {
@@ -15,55 +20,121 @@ class PhysicsEngine {
     update(deltaTime, speed = 1) {
         const dt = deltaTime * speed;
 
+        // Apply physics to every object
         for (let object of this.objects) {
             object.applyGravity(this.gravity);
 
             object.update(dt);
 
             this.checkFloorCollision(object);
+
             this.checkWallCollision(object);
+        }
+
+        // Check object-to-object collisions
+        this.handleObjectCollisions();
+    }
+
+    handleObjectCollisions() {
+        this.collisionCount = 0;
+
+        for (
+            let i = 0;
+            i < this.objects.length;
+            i++
+        ) {
+            for (
+                let j = i + 1;
+                j < this.objects.length;
+                j++
+            ) {
+                const objectA =
+                    this.objects[i];
+
+                const objectB =
+                    this.objects[j];
+
+                const collision =
+                    checkCircleCollision(
+                        objectA,
+                        objectB
+                    );
+
+                if (collision) {
+                    resolveCollision(
+                        objectA,
+                        objectB,
+                        collision
+                    );
+
+                    this.collisionCount++;
+                }
+            }
         }
     }
 
     checkFloorCollision(object) {
-        const floor = this.height - 10;
+        const floor =
+            this.height - 10;
 
         if (
-            object.position.y + object.radius >= floor
+            object.position.y +
+                object.radius >=
+            floor
         ) {
             object.position.y =
                 floor - object.radius;
 
             object.velocity.y =
-                -object.velocity.y * object.restitution;
+                -object.velocity.y *
+                object.restitution;
         }
     }
 
     checkWallCollision(object) {
+
         // Left wall
-        if (object.position.x - object.radius <= 0) {
-            object.position.x = object.radius;
+        if (
+            object.position.x -
+                object.radius <=
+            0
+        ) {
+            object.position.x =
+                object.radius;
 
             object.velocity.x =
-                -object.velocity.x * object.restitution;
+                -object.velocity.x *
+                object.restitution;
         }
 
         // Right wall
-        if (object.position.x + object.radius >= this.width) {
+        if (
+            object.position.x +
+                object.radius >=
+            this.width
+        ) {
             object.position.x =
-                this.width - object.radius;
+                this.width -
+                object.radius;
 
             object.velocity.x =
-                -object.velocity.x * object.restitution;
+                -object.velocity.x *
+                object.restitution;
         }
     }
 
     reset() {
         this.objects = [];
+
+        this.collisionCount = 0;
     }
 
     getObjectCount() {
         return this.objects.length;
+    }
+
+    getCollisionCount() {
+        return this.collisionCount;
     }
 }
 
--- frontend/src/physics/PhysicsObject.js
+++ frontend/src/physics/PhysicsObject.js
@@ -1,39 +1,35 @@
 import Vector2 from "./Vector2";
 
 class PhysicsObject {
+    constructor(x, y, radius = 20) {
+        this.position =
+            new Vector2(x, y);
 
-    constructor(x, y, radius = 30) {
-
-        this.position = new Vector2(x, y);
-
-        this.velocity = new Vector2(0, 0);
-
-        this.acceleration = new Vector2(0, 9.8);
+        this.velocity =
+            new Vector2(0, 0);
 
         this.radius = radius;
 
         this.mass = 1;
 
         this.restitution = 0.75;
+
+        this.friction = 0.2;
     }
 
     applyGravity(gravity) {
-        this.acceleration.y = gravity;
+        this.velocity.y +=
+            gravity * 100;
     }
 
     update(deltaTime) {
-
-        this.velocity.x +=
-            this.acceleration.x * deltaTime;
-
-        this.velocity.y +=
-            this.acceleration.y * deltaTime;
-
         this.position.x +=
-            this.velocity.x * deltaTime;
+            this.velocity.x *
+            deltaTime;
 
         this.position.y +=
-            this.velocity.y * deltaTime;
+            this.velocity.y *
+            deltaTime;
     }
 }
 
